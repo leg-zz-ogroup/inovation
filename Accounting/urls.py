058e76b18cb5e0820b532bdf850ef2b7d56e5c76
Ahmad Mohammad Shafiee <Ahmad Mohammad Shafiee>
2016-09-06 13:04:08 +0430

    migrate to python 3.5

--- Accounting/urls.py
+++ Accounting/urls.py
@@ -1,6 +1,5 @@
-from django.conf.urls import patterns, include, url
 from Apps.BaseData import CounterParty
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
@@ -9,14 +8,13 @@ from Apps.Index import MainPage
 
 admin.autodiscover()
 
-urlpatterns = patterns('',
-                       # Examples:
+urlpatterns = [                       # Examples:
     url(r'^$', MainPage.index_page),
     url(r'^index.html', MainPage.index_page),
                        # url(r'^blog/', include('blog.urls')),
     url(r'^ajax/actualcounterparty/', CounterParty.CounterParty.actual_counterparty_list_maker, ''),
                        # url(r'^admin/', include(admin.site.urls)),
-                       ) + static(settings.STATIC_URL, document_root=settings.STATIC_URL)
+                       ] + static(settings.STATIC_URL, document_root=settings.STATIC_URL)
 
 
 
